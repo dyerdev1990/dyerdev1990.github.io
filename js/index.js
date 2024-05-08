bd103b4a53ee9448e1c7efef1750b2ad5fb380f3
dyerdev1990 <168540936+dyerdev1990@users.noreply.github.com>
2024-05-08 12:59:48 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // ;(function () {
 buildHeader();
 buildFooter();
+fetchContent();
 
 const currYear = new Date().getFullYear();
 
@@ -100,4 +101,37 @@ function buildFooter() {
   parent.innerHTML = footer;
 }
 
+const buildAbout = (content) => {
+  const parent = document.getElementById("aboutMeContent");
+
+  parent.insertAdjacentHTML(
+    "beforeend",
+    content.map((aboutMeContent) => {
+      return `
+        <p class="about__me--text">${aboutMeContent.paragrapOne}</p>
+        <p class="about__me--text">${aboutMeContent.paragrapTwo}</p>
+        <p class="about__me--text">${aboutMeContent.paragrapThree}</p>
+        <p class="about__me--text">${aboutMeContent.paragrapFour}</p>
+    `;
+    })
+  );
+};
+
+function fetchContent() {
+  let PROJECT_ID = "c7jd3ho8";
+  let DATASET = "production";
+  let QUERY = encodeURIComponent('*[_type == "about"]');
+
+  // Compose the URL for your project's endpoint and add the query
+  let URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
+
+  fetch(URL)
+    .then((res) => res.json())
+    .then((response) => {
+      buildAbout(response.result);
+    })
+    .catch((error) => {
+      console.error("THIS WEAS AN ERROR fetching content ");
+    });
+}
 // })()
